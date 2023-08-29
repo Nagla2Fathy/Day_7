2c213a2804b5573e5770a731c1f00e03aa3051c3
Nagla2Fathy <143462330+Nagla2Fathy@users.noreply.github.com>
2023-08-29 23:12:04 +0300

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  logo: string = 'amazon';
+  headerLinks: string[] = ['Home', 'Products',  'About', 'contact us'];
+  language: string = 'en-Us';
+  isLogin:boolean=false;
+  //'ar-SA'
+  constructor() {}
+
+  changeLanguage() {
+    this.language = this.language == 'en-Us' ? 'ar-SA' : 'en-Us';
+  }
+}
+
+
+
